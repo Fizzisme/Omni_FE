920ddd87cd32db0831745eb26c81a3f6b2b860df
Fizzisme <nguyenletuanphi12c1.2022@gmail.com>
2026-03-27 16:50:57 +0700

    Fix layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
-import {getCategoryBySlug} from "@/app/shop/[categoryId]/page";
 
 
 const notoSansSans = Noto_Sans(
